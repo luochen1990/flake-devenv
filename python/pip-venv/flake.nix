f4c9b183773f999f0da42af7c140a3b4b401a5dd
LuoChen <luochen1990@gmail.com>
2024-07-31 22:06:03 +0800

    fix: python/pip-venv

--- python/pip-venv/flake.nix
+++ python/pip-venv/flake.nix
@@ -11,23 +11,24 @@
     eachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
       inherit system;
       pkgs = import nixpkgs { inherit system; };
-      python3 = pkgs.python310; # or pkgs.python310.withPackages (p: []) if you need more python packages in nixpkgs
-      pyver = python3.version;
+      python = pkgs.python310; #TODO: change to the version you want. e.g. `pkgs.python39` for python3.9;
+      # or change to `pkgs.python310.withPackages (p: [])` if you need more python packages in nixpkgs
+      pyver = python.version;
     });
   in
   {
-    devShells = eachSystem ({pkgs, python3, pyver, ...}: rec {
+    devShells = eachSystem ({pkgs, python, pyver, ...}: rec {
       default = pip;
 
       pip = pkgs.mkShell {
         buildInputs = [
-          python3
+          python
           # Add dev dependencies here
-          pkgs.python3Packages.pip
-          pkgs.python3Packages.virtualenv
-          pkgs.python3Packages.setuptools
-          pkgs.python3Packages.wheel
-          pkgs.python3Packages.typing-extensions
+          python.pkgs.pip
+          python.pkgs.virtualenv
+          python.pkgs.setuptools
+          python.pkgs.wheel
+          python.pkgs.typing-extensions
           pkgs.git
         ];
 
@@ -36,12 +37,12 @@
 
           export PROMPT_COMMAND='PS1="\[\033[01;32m\]venv-py${pyver}\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ ";unset PROMPT_COMMAND'
 
-          python3 -m venv --system-site-packages ${builtins.toString ./.venv-py${pyver}}
-          source ${builtins.toString ./.venv-py${pyver}/bin/activate}
+          python3 -m venv --system-site-packages ./.venv-py${pyver}
+          source ./.venv-py${pyver}/bin/activate
 
           export PIP_EXTRA_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
 
-          if [ -f setup.py ] || [ -f setup.cfg ] || [ -f pyproject.toml ]; ; then
+          if [ -f setup.py ] || [ -f setup.cfg ] || [ -f pyproject.toml ]; then
             echo "Project manifest detected. Installing project dependencies..."
             pip install -e .
           elif [ -f requirements.txt ]; then
